6ca4f89d76b2b78a7ced6ab057ecbb32dd2a51b1
Miguel_23300705-cmd <a23300705@ceti.mx>
2026-04-27 22:33:37 -0600

    Update panpri.js

--- frontend/panpri.js
+++ frontend/panpri.js
@@ -35,7 +35,7 @@ function getToken() {
 
 function authHeaders(json = false) {
   const token = getToken();
-  const h = { 'Authorization': `Bearer ${token}` };
+  const h = { 'token': token }; 
   if (json) h['Content-Type'] = 'application/json';
   return h;
 }
